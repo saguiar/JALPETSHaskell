54c5e37dd8ff17a5890831ce67b36b20a30c178b
isabelasgomes <isodregomes@gmail.com>
2017-06-06 01:46:09 +0000

    Implementação do patch no agendamento, alteração de uma faixa de horários

--- src/Agendamento.hs
+++ src/Agendamento.hs
@@ -6,8 +6,10 @@ import Yesod
 import Foundation
 import Control.Monad.Logger (runStdoutLoggingT)
 import Control.Applicative
-import Data.Text
-
+import qualified Data.Text as T
+import Network.HTTP.Types.Status
+import Data.Aeson.Types (emptyObject)
+import Data.Time.Clock
 import Database.Persist.Postgresql
 
 --Cadastro de Agendamento
@@ -25,25 +27,34 @@ getAgendamentosR = do
 
 --patch para alteracao dos agendamentos, o patch pode alterar várias linhas ao mesmo tempo, mas não as colunas, achei que seria útil pra por exemplo, atualizar todos os horários/datas dos agendamentos
 patchAgendamentosR :: Handler Value
-patchAgendamentosR :: undefined
+patchAgendamentosR = do
+    (horarioNovo, limite) <- requireJsonBody :: Handler (NominalDiffTime, UTCTime)
+    agora <- liftIO getCurrentTime
+    ages <- runDB $ selectList [AgendamentoDataa >=. agora, AgendamentoDataa <. limite] []
+ --         mapM_ :: Monad m => (a -> m b) -> [a] ->  m ()
+ --         mapM  :: Monad m => (a -> m b) -> [a] ->  m [b]
+ --         map   ::            (a ->   b) -> [a] ->    [b]
+    runDB $ mapM_ (\(Entity aid age) -> update aid [AgendamentoDataa =. addUTCTime horarioNovo (agendamentoDataa age)]) ages
+    sendStatusJSON noContent204 emptyObject
 
 --get -> lista um agendamento por vez
 getAgendamentoR :: AgendamentoId -> Handler Value
 getAgendamentoR agendamentoId = do
-    aid <- runDB $ get404 AgendamentoId
+    aid <- runDB $ get404 agendamentoId
     sendStatusJSON ok200 (object ["resp" .= aid])
     
 --put ->alteração de um agendamento
 putAgendamentoR :: AgendamentoId -> Handler Value
 putAgendamentoR agendamentoId = do
-    aid <- runDB $ replace agendamentoId
+    agendamento <- requireJsonBody :: Handler Agendamento
+    aid <- runDB $ replace agendamentoId agendamento
     sendStatusJSON ok200 (object ["resp" .= aid])
     
 --delete -> deleta um agendamento
-deleteAgendamentoR :: AgendamentoR -> Handler Value
-deleteAgendamentoR agendamentoR = do
-    aid <- runDB $ delete agendamentoR
-    sendStatusJSON noContent204 (object ["resp" .= aid])
+deleteAgendamentoR :: AgendamentoId -> Handler Value
+deleteAgendamentoR agendamentoId = do
+    aid <- runDB $ delete agendamentoId
+    sendStatusJSON noContent204 emptyObject
     
     
     
